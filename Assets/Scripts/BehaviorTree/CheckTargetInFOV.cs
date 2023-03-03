3c1dc56f5fece969da73e21db596a31796797d12
Dann Kaoussarani <danjaf2@gmail.com>
2023-03-03 14:42:20 -0500

    Made a way to reference to parent GameObject while inside the nodes
    In order to be able to grab transforms

---
+++ Assets/Scripts/BehaviorTree/CheckTargetInFOV.cs
@@ -0,0 +1,37 @@
+using BehaviorTree;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTargetInFOV : Node
+{
+    // Start is called before the first frame update
+    float viewAngle = 70f;
+    public CheckTargetInFOV()
+    {
+        //Constructor for setting up base values in Example Character Tree
+    }
+
+    public override NodeState Evaluate()
+    {
+        Transform target = (Transform)GetData("target");
+        if (target != null)
+        {
+            Vector3 toTarget = target.position - referenceTree.transform.position;
+            if (Vector3.Angle(referenceTree.transform.forward, toTarget) <= viewAngle)
+            {
+                if (Physics.Raycast(referenceTree.transform.position, toTarget, out RaycastHit hit))
+                {
+                    if(hit.transform.root == target)
+                    {
+                        state = NodeState.SUCCESS;
+                        return state;
+                    }
+                }
+            }
+        }
+        //Evaluation of state and apply transformation changes
+        state = NodeState.FAILURE;
+        return state;
+    }
+}
