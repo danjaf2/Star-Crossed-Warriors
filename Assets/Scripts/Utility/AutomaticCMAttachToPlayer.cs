bafda5ae1b8f3350e506e5812d5692805cff1700
lncosta <luiza.n16@gmail.com>
2023-03-10 23:44:31 -0500

    Player Classes Prefabs

---
+++ Assets/Scripts/Utility/AutomaticCMAttachToPlayer.cs
@@ -0,0 +1,22 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticCMAttachToPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CinemachineVirtualCamera virtualCamera; 
+    void Start()
+    {
+       virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        virtualCamera.Follow = GameObject.FindGameObjectWithTag("PlayerFollowCamera").transform;
+        virtualCamera.LookAt = GameObject.FindGameObjectWithTag("PlayerCockpit").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
