4656f90cc10ab422019776632c02e9face24fd9e
Dann Kaoussarani <danjaf2@gmail.com>
2023-03-19 21:03:37 -0400

    Small improvement to Obstacle Avoidance to make it so it can adapt to new obstacles

--- Assets/Scripts/AI/Movements/ObstacleCollisionAvoidance.cs
+++ Assets/Scripts/AI/Movements/ObstacleCollisionAvoidance.cs
@@ -8,6 +8,7 @@ public class ObstacleCollisionAvoidance : AIMovement
     public LayerMask layerMask;
     public float avoidanceDistanceFactor = 0.1f;
     public float avoidanceDistanceMinimum = 250;
+    public GameObject objectToAvoid=null;
     public float avoidanceTime = 4f;
     bool avoiding = false;
     Quaternion prevRotation;
@@ -21,13 +22,7 @@ public class ObstacleCollisionAvoidance : AIMovement
             rb= GetComponent<Rigidbody>();
         }
         var output = base.GetKinematic(agent);
-        if(avoiding)
-        {
-            output.linear = prevDirection;
-            output.angular = prevRotation;
-            Debug.DrawRay(transform.position, output.linear * 20, Color.green);
-            return output;
-        }
+        
         Vector3 desiredVelocity = Vector3.zero;
         RaycastHit hit;
         //Debug.DrawRay(transform.position, rb.velocity * 20, Color.yellow);
@@ -46,8 +41,16 @@ public class ObstacleCollisionAvoidance : AIMovement
         }
         if (Physics.CapsuleCast(transform.position, transform.position + checkDirection.normalized, 20, checkDirection, out hit, distance, layerMask, QueryTriggerInteraction.Collide))
         {
+            if (avoiding&&hit.transform.gameObject==objectToAvoid)
+            {
+                output.linear = prevDirection;
+                output.angular = prevRotation;
+                Debug.DrawRay(transform.position, output.linear * 20, Color.green);
+                return output;
+            }
+
             desiredVelocity = Vector3.Cross(hit.normal, transform.forward);
-            
+            objectToAvoid = hit.transform.gameObject;
             output.linear = desiredVelocity * weight;
             output.angular = Quaternion.LookRotation(desiredVelocity);
             prevRotation = output.angular; 
@@ -79,6 +82,7 @@ public class ObstacleCollisionAvoidance : AIMovement
         GetComponent<Seek>().canPerform = false;
         GetComponent<FaceTarget>().canPerform = false;
         yield return new WaitForSeconds(avoidanceTime);
+        objectToAvoid= null;
         avoiding= false;
         GetComponent<Seek>().canPerform = true;
         GetComponent<FaceTarget>().canPerform = true;
