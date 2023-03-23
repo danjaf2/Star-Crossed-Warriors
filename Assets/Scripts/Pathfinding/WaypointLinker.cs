42ebbdfe2f961547dd6f1c56dc90c967a53bf13c
Dann Kaoussarani <danjaf2@gmail.com>
2023-03-23 12:51:35 -0400

    WayPoint spawner(points get saved but not their connections)

--- Assets/Scripts/Pathfinding/WaypointLinker.cs
+++ Assets/Scripts/Pathfinding/WaypointLinker.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class WaypointLinker : EditorWindow
 {
     public List<Waypoint> waypoints;
+
+
+    public Object waypointPrefab;
+    public Vector3 startingPos = Vector3.zero;
+    public int numberRows = 10;
+    public int numberColumn =10;
+    public int zDepth = 10;
+    public float spacing=60;
+
     [MenuItem("Window/Edit Mode Functions")]
     public static void ShowWindow()
     {
@@ -20,6 +30,10 @@ public class WaypointLinker : EditorWindow
         else if (GUILayout.Button("Clear Waypoints"))
         {
             ClearWaypoints();
+        }else if(GUILayout.Button("Spawn Waypoints"))
+        {
+            waypointPrefab= (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Waypoints/Waypoint.prefab", typeof(GameObject));
+            SpawnWaypoints();
         }
     }
 
@@ -32,11 +46,42 @@ public class WaypointLinker : EditorWindow
         }
     }
 
-
-
-        private void FunctionToRun()
+    private void SpawnWaypoints()
     {
         waypoints = GameObject.FindGameObjectWithTag("Manager").GetComponent<Rooms>().waypoints;
+        waypoints.Clear();
+        GameObject parentObject = GameObject.Find("FixedWaypoints");
+        numberRows = 10;
+        numberColumn = 10;
+        zDepth = 10;
+        spacing= 60;
+        for (int row =0;row<numberRows;row++)
+        {
+            for (int col = 0; col < numberColumn; col++)
+            {
+                for (int depth = 0; depth < zDepth; depth++)
+                {
+                    Vector3 position = new Vector3(startingPos.x + row * spacing, startingPos.y + col * spacing, startingPos.z + depth * spacing);
+                    Debug.Log(position);
+                    GameObject point = new GameObject();
+                    point = PrefabUtility.ConnectGameObjectToPrefab(point, waypointPrefab as GameObject);
+                    //GameObject point = ((GameObject)PrefabUtility.InstantiatePrefab(waypointPrefab));
+                    point.transform.position = position;
+                    point.transform.parent = parentObject.transform;
+                    point.gameObject.name = point.gameObject.name + "_" + row+ "_" + col+ "_" + depth;
+                    waypoints.Add(point.GetComponent<Waypoint>());
+                }
+            }
+        }
+    }
+
+
+
+    private void FunctionToRun()
+    {
+        waypoints = GameObject.FindGameObjectWithTag("Manager").GetComponent<Rooms>().waypoints;
+        spacing= 60;
+        float distanceLimit = Mathf.Sqrt((Mathf.Pow(60, 2) + Mathf.Pow(60, 2)));
         foreach (var w in waypoints)
         {
 
@@ -66,6 +111,7 @@ public class WaypointLinker : EditorWindow
                     if (hit.transform.tag != "Wall" && hit.transform.tag != "Ground" &&hit.transform.position == w.position)
 
                     {
+                        if(hit.distance < distanceLimit) { 
                         Debug.DrawLine(current.position, hit.point, Color.cyan, 9f);
 
                         if (!w.connectedTo.Contains(current))
@@ -81,7 +127,7 @@ public class WaypointLinker : EditorWindow
 
 
                         continue;
-
+                        }
                     }
                     else
                     {
