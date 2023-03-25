c48004c3568acefad7e4b16f1b60bed8079d0122
Étienne <e_acine@live.concordia.ca>
2023-03-25 02:16:29 -0400

    Minute Optimization
    It seems to me that this script should be two different classes.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,19 +6,19 @@ public class CameraFollow : MonoBehaviour
 {
 
     [SerializeField] public GameObject target;
-    [SerializeField] public Vector3 positionFromTarget; 
+    [SerializeField] public Vector3 positionFromTarget;
+    bool _camMode;
     // Start is called before the first frame update
     void Start()
     {
         //positionFromTarget = new Vector3(0, 0, 0); 
+        _camMode = transform.tag != "Radar";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-        if(transform.tag != "Radar")
+        if(_camMode)
         {
             transform.position = target.transform.position;
             transform.rotation = target.transform.rotation;
@@ -29,9 +29,5 @@ public class CameraFollow : MonoBehaviour
             //transform.position = target.GetComponent<Rigidbody>().centerOfMass + positionFromTarget;
             transform.rotation = Quaternion.Euler(90, 0, target.transform.rotation.eulerAngles.z);
         }
-       
-        
     }
-
-    
 }
