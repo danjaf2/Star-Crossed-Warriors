94ce5904e5ee8f2a1c50d2dd40d08827964b25e8
lncosta <luiza.n16@gmail.com>
2023-04-05 19:11:53 -0400

    Custom laser material

--- Assets/Scripts/Combat/Entities/HeavyShip.cs
+++ Assets/Scripts/Combat/Entities/HeavyShip.cs
@@ -8,8 +8,36 @@ public class HeavyShip : PlayerShip {
     //primaryFireRate: (continuous)
     //lockOnRate: 100;
 
+    [Header("Bullet")]
+    [SerializeField] Bullet _bulletPrefab;
+    [SerializeField] float _bulletDamage;
+    [SerializeField] float _bulletSpeed;
+    [SerializeField] float _bulletDelay;
+    [SerializeField] GameObject spawnPosition;
+    float _fireTimer = 5.0f;
+
+    [Header("Missile")]
+    [SerializeField] HomingMissile _missilePrefab;
+
     public override void HandleShoot(bool input) {
         // LASER BEAM!!!
+
+        if (_fireTimer > 0) { _fireTimer--; }
+
+        if (input && _fireTimer <= 0)
+        {
+            Attack bulletAttack = new Attack(_bulletDamage, this);
+            bulletAttack.OnHit += ReactToBulletHit;
+
+            Bullet newBullet = Bullet.Create(
+                _bulletPrefab,
+                bulletAttack,
+                spawnPosition.transform.position,
+                this.transform.forward * _bulletSpeed
+            );
+
+            _fireTimer = _bulletDelay;
+        }
     }
 
     public override void HandleMissile(bool input) {
@@ -18,5 +46,11 @@ public class HeavyShip : PlayerShip {
 
     public override void HandleAbility(bool input) {
         //  replusion field / reflective shields
+    }
+
+    private void ReactToBulletHit(Attack atk, Entity hit)
+    {
+        hit.AddEffect(new FragileEffect(hit));
+        Debug.Log($"Heavy knows that {hit.name} was hit for {atk.Damage} damage.\nApplied a fragile debuff.");
     }
 }
