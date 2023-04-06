1984e03f19c2ef410b496a0e147c176e14362c6d
lncosta <luiza.n16@gmail.com>
2023-04-05 23:11:33 -0400

    Enemy classes base

---
+++ Assets/Scripts/Combat/Entities/Enemies/Mothership.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mothership : EnemyShip
+{
+    public override void HandleAbility(bool input)
+    {
+        //Shields
+        throw new System.NotImplementedException();
+    }
+
+    public override void HandleMissile(bool input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HandleShoot(bool input)
+    { //EMP
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Combat/Entities/Enemies/Sentry.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sentry : EnemyShip
+{
+    public override void HandleAbility(bool input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HandleMissile(bool input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HandleShoot(bool input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
