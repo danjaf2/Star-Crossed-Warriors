ecb57cf5e0360006ba27cd82311669e04a103e0b
rex884 <mikeyrex884@gmail.com>
2023-04-08 11:33:18 -0400

    Added TestRelay C# Script

---
+++ Assets/Scripts/TestRelay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+
+public class TestRelay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+
+        AuthenticationService.Instance.SignedIn += () =>
+         {
+             Debug.Log("Signed in "+ AuthenticationService.Instance.PlayerId);
+         };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    
+    private async void CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+        }catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+        
+    }
+    private void JoinRelay(string joinCode)
+    {
+
+    }
+}
