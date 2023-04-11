5e408d0c89667ead23c9c3a61f1f2ea29b8f863f
Étienne <e_acine@live.concordia.ca>
2023-04-11 17:58:19 -0400

    Epic Awesome Super Great fix

---
+++ Assets/Scripts/EpicNetworkManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Unity.Netcode;
+
+public class EpicNetworkManager : NetworkManager {
+    public bool ClientConnected { get => _clientConnected; }
+    bool _clientConnected;
+
+    public static EpicNetworkManager Instance;
+
+    private void Awake() {
+        Instance = this;
+        OnClientConnectedCallback += (o) => { _clientConnected = true; };
+    }
+}
