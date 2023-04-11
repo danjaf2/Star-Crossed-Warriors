c2e353f5107efb4807756293104bb9ec97c38b8a
rex884 <mikeyrex884@gmail.com>
2023-04-11 01:41:22 -0400

    Added Relay and Quantum Console

--- Assets/Scripts/TestRelay.cs
+++ Assets/Scripts/TestRelay.cs
@@ -5,10 +5,15 @@ using Unity.Services.Core;
 using Unity.Services.Authentication;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
+using Unity.Netcode.Transports.UTP;
+using QFSW.QC;
+using Unity.Networking.Transport.Relay;
+using Unity.Netcode;
 
 public class TestRelay : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] public GameObject characterSelectPanel;
     private async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -21,7 +26,7 @@ public class TestRelay : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    
+    [Command]
     private async void CreateRelay()
     {
         try
@@ -29,20 +34,34 @@ public class TestRelay : MonoBehaviour
             Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
 
             string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-            Debug.Log(joinCode);
+
+
+            Debug.Log("**********************************************JOIN CODE IS "+joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            characterSelectPanel.SetActive(false);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartHost();
         }catch (RelayServiceException e)
         {
             Debug.Log(e);
         }
         
     }
+    [Command]
     private async void JoinRelay(string joinCode)
     {
         try
         {
             Debug.Log("Joining Relay with " + joinCode);
-            await RelayService.Instance.JoinAllocationAsync(joinCode);
-        }catch(RelayServiceException e)
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            characterSelectPanel.SetActive(false);
+            NetworkManager.Singleton.StartClient();
+        }
+        catch(RelayServiceException e)
         {
             Debug.Log(e);
         }
